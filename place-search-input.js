ab2fe395270cee29cb373d46b27e1c05f42b9980
Mike Comstock <mikecomstock@gmail.com>
2012-03-17 00:22:32 -0400

    initial import of PlaceSearchInput

---
+++ place-search-input.js
@@ -0,0 +1,26 @@
+﻿MC.Views.PlaceSearchInput = Backbone.View.extend({
+    initialize: function () {
+
+        this.geocoder = new google.maps.Geocoder();
+
+        var self = this;
+        this.source = function (request, response) {
+            self.geocoder.geocode({ 'address': request.term }, function (results, status) {
+
+                results.forEach(function (r) {
+                    r.label = r.formatted_address;
+                });
+
+                response(results);
+            });
+        };
+
+        this.autocomplete = this.$el.autocomplete({ source: this.source, autoFocus: true });
+    },
+    events: {
+        'autocompleteselect': 'autocompleteselect'
+    },
+    autocompleteselect: function (event, ui) {
+        this.trigger('select', ui.item);
+    }
+});
